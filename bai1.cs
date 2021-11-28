194126afc1bf598bafb0b6054a63056845b3a6b9
Thangga7 <beencules@gmail.com>
2021-11-28 03:45:01 -0700

    Basicc#

---
+++ bai1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Bai_Tap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int x;
+        int y;
+        for (x = -10; x <= 10; x++)
+        {
+            y = x*x - 2*x +1;
+
+            Console.WriteLine(y);
+            
+        }
+        }
+    }
+}
